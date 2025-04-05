19b6ab5e26eb9910ff42c692d74704aed5c2c7ce
jtang12-cscc <jtang12@student.cscc.edu>
2025-04-05 15:53:43 -0400

    Added ability to fire ammo and having a limit to the amount

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
 
 class AlienInvasion:
 
@@ -22,7 +23,11 @@ class AlienInvasion:
         self.running = True
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound('laser.mp3')
+        self.laser_sound.set_volume(0.7)
+
+        self.ship = Ship(self, Arsenal(self))
     
     def run_game(self):
         # Game loop
@@ -60,11 +65,17 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                self.laser_sound.play()
+                self.laser_sound.fadeout(160)
+
         elif event.key == pygame.K_q:
             self.running = False
             pygame.quit()
             sys.exit()
+        
 
 if __name__ == '__main__':
-    ai = AlienInvasion()
-    ai.run_game()
+    AlienInvasion = AlienInvasion()
+    AlienInvasion.run_game()
--- ship.py
+++ ship.py
@@ -1,10 +1,15 @@
 import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
+
 
 class Ship:
-    """A class to manage the ship."""
 
-    def __init__(self, game):
-        """Initialize the ship and set its starting position."""
+
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -20,9 +25,14 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
         self.x = float(self.rect.x)
+        self.arsenal = arsenal
     
     def update(self):
         # Updating position of the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
@@ -32,4 +42,8 @@ class Ship:
         self.rect.x = self.x
 
     def blitme(self):
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        return self.arsenal.fire_bullet()
