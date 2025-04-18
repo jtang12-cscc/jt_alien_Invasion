3629b296b9db1a0cd5a2f3cf2fb32126ad689855
jtang12-cscc <jtang12@student.cscc.edu>
2025-04-18 17:11:50 -0400

    p3 - split settings into dynamic and statiic added increase difficulty function

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.difficulty_scale = 1.1
 
         # Image courtesy of jagdos of OpenGameArt.org. Original name is simply "Rocket", and this
         # is a public domain image.
@@ -15,16 +16,10 @@ class Settings:
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'onlyrocket.png'
         self.ship_w = 60
         self.ship_h = 150
-        self.ship_speed = 5
-        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
-        self.bullet_speed = 7
-        self.bullet_w = 25
-        self.bullet_h = 80
-        self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_w = 40
@@ -41,3 +36,20 @@ class Settings:
         self.button_font_size = 48
         self.HUD_font_size = 20
         self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.starting_ship_count = 3
+
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+
+        self.fleet_speed = 2
+        self.fleet_drop_speed = 40
+
+    def increase_difficulty(self):
+        self.ship_speed += self.difficulty_scale
+        self.bullet_speed += self.difficulty_scale
+        self.fleet_speed += self.difficulty_scale
