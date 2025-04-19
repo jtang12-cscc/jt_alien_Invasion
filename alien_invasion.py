1ce9b9183bcfb309c5aea49e065647ca6d8e52a2
jtang12-cscc <jtang12@student.cscc.edu>
2025-04-18 22:47:08 -0400

    p3 - fleshed out games states to be updated by the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,7 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self.settings.starting_ship_count)
+        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w,self.settings.screen_h)
@@ -70,11 +70,13 @@ class AlienInvasion:
         if collisions:
             self.impact.play()
             self.impact.fadeout(500)
+            self.game_stats.update(collisions)
         
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
             self.settings.increase_difficulty()
             # update game stats level
+            self.game_stats.update_level()
             # update HUD view
 
 
@@ -97,7 +99,8 @@ class AlienInvasion:
     
     def restart_game(self):
         self.settings.initialize_dynamic_settings()
-        # resets Game settings
+        # resets Game stats
+        self.game_stats.reset_stats()
         # update HUD scores
         self._reset_level()
         self.ship._center_ship()
@@ -109,7 +112,7 @@ class AlienInvasion:
         self.ship.blitme()
         self.alien_fleet.draw()
         # draw HUD
-        
+
 
         if not self.game_active:
             self.play_button.draw()
