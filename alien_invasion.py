5cfea225e63188e4c3243dad424e025fe73b8edb
jtang12-cscc <jtang12@student.cscc.edu>
2025-04-19 22:32:33 -0400

    p3 - created HUD to display score, max, and hi score with persistent file storage

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,7 @@ from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from time import sleep
 from button import Button
+from hud import HUD
 
 class AlienInvasion:
 
@@ -15,7 +16,6 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.settings.initialize_dynamic_settings()
-        self.game_stats = GameStats(self)
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w,self.settings.screen_h)
@@ -27,6 +27,8 @@ class AlienInvasion:
             (self.settings.screen_w, self.settings.screen_h)
             )
 
+        self.game_stats = GameStats(self)
+        self.HUD = HUD(self)
         self.running = True
         self.clock = pygame.time.Clock()
 
@@ -71,6 +73,7 @@ class AlienInvasion:
             self.impact.play()
             self.impact.fadeout(500)
             self.game_stats.update(collisions)
+            self.HUD.update_scores()
         
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
@@ -99,9 +102,8 @@ class AlienInvasion:
     
     def restart_game(self):
         self.settings.initialize_dynamic_settings()
-        # resets Game stats
         self.game_stats.reset_stats()
-        # update HUD scores
+        self.HUD.update_scores()
         self._reset_level()
         self.ship._center_ship()
         self.game_active = True
@@ -111,7 +113,7 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.blitme()
         self.alien_fleet.draw()
-        # draw HUD
+        self.HUD.draw()
 
 
         if not self.game_active:
@@ -124,6 +126,7 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+                self.game_stats.save_scores()
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.KEYDOWN and self.game_active == True:
@@ -157,6 +160,7 @@ class AlienInvasion:
 
         elif event.key == pygame.K_q:
             self.running = False
+            self.game_stats.save_scores()
             pygame.quit()
             sys.exit()
         
--- game_stats.py
+++ game_stats.py
@@ -16,7 +16,7 @@ class GameStats():
 
     def init_saved_scores(self):
         self.path = self.settings.scores_file
-        if self.path.exists() and self.path.stat.__sizeof__() > 80:
+        if self.path.exists() and self.path.stat.__sizeof__() > 20:
             contents = self.path.read_text()
             scores = json.loads(contents)
             self.hi_score = scores.get('hi_score', 0)
@@ -48,7 +48,7 @@ class GameStats():
         self._update_max_score()
 
         # update hi_score
-        self._update_hi_score
+        self._update_hi_score()
 
     def _update_max_score(self):
         if self.score > self.max_score:
